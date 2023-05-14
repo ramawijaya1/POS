17ebd600ece2fcbcfb40dbb4df7f7efb5e3af5ee
Zhafirah350 <zhafirah.arinusi3005@gmail.com>
2023-05-14 21:42:10 +0700

    ganti judul web sing nde atas

--- Admin/login.php
+++ Admin/login.php
@@ -35,7 +35,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Aplikasi Kasir | by.adgrafika</title>
+    <title>Login Aplikasi Kasir | by.kelompok5</title>
     <link rel="icon" href="favicon.ico">
     <link rel="icon" href="favicon.ico" type="image/ico">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
